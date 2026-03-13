40f65113932f2028fb0dd52d47180ba346dd1efe
Manar <esdibmanar@gmail.com>
2026-03-13 11:23:29 +0000

    gmjhghg

--- Library.MVC/Models/Book.cs
+++ Library.MVC/Models/Book.cs
@@ -3,10 +3,10 @@
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Isbn { get; set; }
-        public string Category { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Isbn { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
         public bool IsAvailable { get; set; }
         public ICollection<Loan> Loans { get; set; } = new List<Loan>();
     }
--- Library.MVC/Models/Member.cs
+++ Library.MVC/Models/Member.cs
@@ -3,9 +3,9 @@
     public class Member
     {
         public int Id { get; set; }
-        public string FullName { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
         public ICollection<Loan> Loans { get; set; } = new List<Loan>();
     }
 }
